37fbf71795766e332fbd268ada2778ffc5066ef0
Dima <katorgin.dmitry@gmail.com>
2021-10-25 19:42:44 +0300

    minor fixes

--- src/components/ServerIssues.jsx
+++ src/components/ServerIssues.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {Typography} from 'antd';
 import serverIssuesSvg from '../images/serverDown.svg'
-
-const { Title, Text } = Typography;
 
 const ServerIssues = () => {
     return (
         <div className="server-issues">
-            <Title level={2}>Ooops! Looks like there's some propblem on the server.</Title>
+            <h2>Ooops! Looks like there's some propblem on the server.</h2>
             <p>We're really sorry about that :(</p>
             <p>Please try again later.</p>
             <img src={serverIssuesSvg} alt="server problems" style={{display: 'block', maxWidth: '100%'}}/>
