ae7650ec8c9c5d071c86a8ef97d368ba7afbcd5e
dimahvatit <katorgin.dmitry@gmail.com>
2022-12-02 23:16:31 +0300

    fix[Homepage]: check globalStats for undefined

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -8,23 +8,21 @@ import './style.css';
 
 const { Title } = Typography;
 
-
 function Homepage() {
+    const { data, isFetching } = useGetCoinsQuery(10);
+    const globalStats = data?.data?.stats;
 
-const { data, isFetching } = useGetCoinsQuery(10);
-const globalStats = data?.data?.stats;
-
-if (isFetching) return <Loader />;
+    if (isFetching) return <Loader />;
 
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row gutter={[0, 16]}>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges, {precision: 1})} /></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap, {precision: 1})} /></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume, {precision: 1})} /></Col>
-                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets, {precision: 1})} /></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats?.total} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats?.totalExchanges, {precision: 1})} /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats?.totalMarketCap, {precision: 1})} /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats?.total24hVolume, {precision: 1})} /></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats?.totalMarkets, {precision: 1})} /></Col>
             </Row>
 
             <div className="home-heading-container">
